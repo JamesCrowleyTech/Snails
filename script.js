77832673894ac4d5a9572935fd8285982e502b88
JamesCrowleyTech <76074300+JamesCrowleyTech@users.noreply.github.com>
2021-03-30 23:16:09 +0100

    Now shows time since article was written

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const timeSinceEl = document.querySelector(".last-edit");
+lastEdit = 1617142207943;
+daysSince = (Date.now() - lastEdit) / 86400000;
+let displayedDate;
 
+if (daysSince < 1) {
+    displayedDate = "Today";
+} else if (1 < daysSince && daysSince < 2) {
+    displayedDate = "Yesterday";
+} else {
+    displayedDate = `${Math.floor(daysSince)} Days Ago`;
+}
+timeSinceEl.textContent = displayedDate;
+
+const section1 = document.querySelector(".section--1");
+const section2 = document.querySelector(".section--2");
+const section3 = document.querySelector(".section--3");
