5e1082bd2675f64f7ba5974e6fa2f245419fcefc
JamesCrowleyTech <76074300+JamesCrowleyTech@users.noreply.github.com>
2021-04-01 21:17:47 +0100

    Implemented smooth scrolling to the sections.

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+"use strict";
+
 const timeSinceEl = document.querySelector(".last-edit");
-lastEdit = 1617142207943;
-daysSince = (Date.now() - lastEdit) / 86400000;
+const lastEdit = 1617308182946;
+const daysSince = (Date.now() - lastEdit) / 86400000;
+
+const [section1, section2, section3] = document.querySelectorAll(".section");
+const contentsSection = document.querySelector(".contents");
+
+const introScroll = (options) => section1.scrollIntoView(options);
+const habitatScroll = (options) => section2.scrollIntoView(options);
+const dietScroll = (options) => section3.scrollIntoView(options);
+const contentsScroll = (options) => contentsSection.scrollIntoView(options);
+
+// prettier-ignore
+const [navBtn1, navBtn2, navBtn3, navBtn4, contentsBtn1, contentsBtn2, contentsBtn3] = [
+    ...document.querySelectorAll(".nav--btn"),
+    ...document.querySelectorAll(".contents-link")];
+const btnOptions = { behavior: "smooth" };
 let displayedDate;
 
 if (daysSince < 1) {
@@ -11,3 +27,13 @@ if (daysSince < 1) {
     displayedDate = `${Math.floor(daysSince)} Days Ago`;
 }
 timeSinceEl.textContent = displayedDate;
+
+navBtn1.addEventListener("click", () => introScroll(btnOptions));
+navBtn2.addEventListener("click", () => habitatScroll(btnOptions));
+navBtn3.addEventListener("click", () => dietScroll(btnOptions));
+navBtn4.addEventListener("click", () => contentsScroll(btnOptions));
+
+contentsBtn1.addEventListener("click", () => introScroll(btnOptions));
+contentsBtn2.addEventListener("click", () => habitatScroll(btnOptions));
+contentsBtn3.addEventListener("click", () => dietScroll(btnOptions));
+
